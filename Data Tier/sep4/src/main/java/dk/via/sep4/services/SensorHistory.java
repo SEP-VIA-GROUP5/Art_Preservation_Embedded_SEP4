feb50f27dbc69535e48e824e997f4e39760ae6b8
AlinaChelmus <alinasorina19@gmail.com>
2022-05-06 18:36:37 +0200

    Data group work

---
+++ Data Tier/sep4/src/main/java/dk/via/sep4/services/SensorHistory.java
@@ -0,0 +1,19 @@
+package dk.via.sep4.services;
+
+import dk.via.sep4.model.charts.DataToSendHistory;
+
+/**
+ * @author $(Alina Chelmus)
+ */
+public class SensorHistory
+{
+
+  public SensorHistory(){
+
+
+  }
+  public DataToSendHistory getSensorsHistoryByPeriod(long id, int period) {
+    DataToSendHistory sendHistories = new DataToSendHistory();
+    return sendHistories;
+  }
+}

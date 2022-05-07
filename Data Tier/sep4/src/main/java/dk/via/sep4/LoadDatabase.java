c81660cfa2d1a9090f5a56c35ac811fbd97ea5d7
AlinaChelmus <alinasorina19@gmail.com>
2022-05-07 12:57:29 +0200

    Room Controller mapping

--- Data Tier/sep4/src/main/java/dk/via/sep4/LoadDatabase.java
+++ Data Tier/sep4/src/main/java/dk/via/sep4/LoadDatabase.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LoadDatabase {
-
+ private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
  @Bean
  CommandLineRunner initDatabase( RoomRepository roomRepository) {
@@ -25,7 +25,7 @@ public class LoadDatabase {
    Sensor s1 = new Sensor(SensorModel.CO2, "PPM", r, 0, 0);
    Sensor s2 = new Sensor(SensorModel.Humidity, "%", r, 0, 0);
    Sensor s3 = new Sensor(SensorModel.Temperature, "°C", r, 0, 20);
-
+   log.info("Preloading " + roomRepository.save(r));
   };
 
  }
--- Data Tier/sep4/src/main/java/dk/via/sep4/models/room/RoomController.java
+++ Data Tier/sep4/src/main/java/dk/via/sep4/models/room/RoomController.java
@@ -1,4 +1,29 @@
 package dk.via.sep4.models.room;
 
+import org.apache.catalina.LifecycleState;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class RoomController {
+
+  private final RoomRepository roomRepository;
+
+  public RoomController(RoomRepository roomRepository)
+  {
+    this.roomRepository = roomRepository;
+  }
+
+  @GetMapping("/room") List<Room> all(){
+    return roomRepository.findAll();
+  }
+
+  @GetMapping("/room/{id}")
+  Room one(@PathVariable java.lang.Long id){
+    return roomRepository.findById(id).orElseThrow( () -> new RoomNotFoundException(id));
+
+  }
 }

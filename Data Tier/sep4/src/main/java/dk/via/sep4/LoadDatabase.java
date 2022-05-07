e34be4a1a10a9cf44a93844f684213c4ee1cc6b0
2945245 <87025282+2945245@users.noreply.github.com>
2022-05-07 10:38:14 +0200

    LoadDatabase implm.

--- Data Tier/sep4/src/main/java/dk/via/sep4/LoadDatabase.java
+++ Data Tier/sep4/src/main/java/dk/via/sep4/LoadDatabase.java
@@ -15,11 +15,17 @@ import org.springframework.context.annotation.Configuration;
 public class LoadDatabase {
  private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-// @Bean
-// CommandLineRunner initDatabase(final String data) {
-//  return args -> {
-//
+ @Bean
+ CommandLineRunner initDatabase(final String data, RoomRepository roomRepository) {
+  return args -> {
+
 //   log.info("Preloading " + data.save());
+   Room r = new Room("Main room");
+   roomRepository.save(r);
+
+   Sensor s1 = new Sensor(SensorType.CO2, "PPM", r, 0, 0);
+   Sensor s2= new Sensor(SensorType.Humidity, "%", r, 0, 0);
+   Sensor s3 = new Sensor(SensorType.Temperature, "°C", r, 0, 20);
 //
 //  };
 // }

6f4f22963d7ba479122a97a12bfd966788cd89b2
2945245 <87025282+2945245@users.noreply.github.com>
2022-05-19 16:32:48 +0200

    Created connection class

---
+++ Data Tier/sep4/src/main/java/dk/via/sep4/dbConnection/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package dk.via.sep4.dbConnection;
+
+import org.jboss.logging.Logger;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+
+public class DatabaseConnection {
+
+    private Logger logger;
+
+   /* private static RabbitProperties.Cache.Connection getRemoteConnection(){
+
+
+    }*/
+}
+

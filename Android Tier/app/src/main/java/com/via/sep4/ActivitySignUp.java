85ab6f4043778dbee26df93053f1e59f63ed6012
sabinapene <102653207+sabinapene@users.noreply.github.com>
2022-05-19 11:17:36 +0200

    Add files via upload

---
+++ Android Tier/app/src/main/java/com/via/sep4/ActivitySignUp.java
@@ -0,0 +1,17 @@
+package com.via.sep4;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.via.sep4.R;
+
+public class ActivitySignUp extends AppCompatActivity {
+
+@Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_signup);
+    //here
+    }
+
+}

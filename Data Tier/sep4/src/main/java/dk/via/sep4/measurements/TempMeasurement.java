f8398c626a4c60016ed0f51d8e4056366e7a567d
AlinaChelmus <alinasorina19@gmail.com>
2022-05-20 13:32:04 +0200

    co2Measurement, hexaConverter,humidity and temperature class, data team

---
+++ Data Tier/sep4/src/main/java/dk/via/sep4/measurements/TempMeasurement.java
@@ -0,0 +1,27 @@
+package dk.via.sep4.measurements;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Ravneet
+ */
+@Entity(name="Temperature")
+@Table(name="temperature")
+public class TempMeasurement {
+  @Id
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  public TempMeasurement() {
+  }
+
+
+  public void HumidityMeasurement() {
+  }
+
+  public TempMeasurement(int temperature) {
+  }
+}
--- Data Tier/sep4/src/main/java/dk/via/sep4/models/LoraWanConnection/HexaConverter.java
+++ Data Tier/sep4/src/main/java/dk/via/sep4/models/LoraWanConnection/HexaConverter.java
@@ -1,16 +1,45 @@
 package dk.via.sep4.models.LoraWanConnection;
 
+import dk.via.sep4.measurements.CO2Measurement;
+import dk.via.sep4.measurements.HumidityMeasurement;
+import dk.via.sep4.measurements.TempMeasurement;
 import dk.via.sep4.models.Sensor.Sensor;
 
 /**
- * @author $(Alina Chelmus)
+ * Ravneet
  */
-public class HexaConverter
-{
-  /*
-  public Sensor convertFromHexToInt(DataReceivedMessage dataReceivedMessage)
+public class HexaConverter {
+
+  private String data;
+
+  public HexaConverter(String data)
   {
+    this.data = data;
+  }
 
 
-  }*/
+
+  public Sensor convertFromHexaToInt(DataReceivedMessage data)
+  {
+    int Co2 ;
+    int temperature;
+    int humidity;
+
+
+    String hexValCo2 = data.getData().substring(0, 4);
+    Co2 = Integer.parseInt(hexValCo2, 16);
+    CO2Measurement co2Measurement = new CO2Measurement(Co2);
+
+    String hexValTemp = data.getData().substring(4,8);
+    temperature = Integer.parseInt(hexValTemp, 16);
+    TempMeasurement temperatureMeasurement = new TempMeasurement(temperature);
+
+    String hexValHum = data.getData().substring(8,12);
+    humidity = Integer.parseInt(hexValHum, 16);
+    HumidityMeasurement humidityMeasurement = new HumidityMeasurement(humidity);
+
+    Sensor sensor = new Sensor();
+
+    return sensor;
+  }
 }

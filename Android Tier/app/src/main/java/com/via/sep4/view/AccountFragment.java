9ef5f82146bfdb2e98fc021896b2611aa9a8bdd0
felixioan <74063400+felixioan@users.noreply.github.com>
2022-05-20 03:59:44 +0200

    Forgot Password created, possible error in layout

--- Android Tier/app/src/main/java/com/via/sep4/view/AccountFragment.java
+++ Android Tier/app/src/main/java/com/via/sep4/view/AccountFragment.java
@@ -28,4 +28,18 @@ public class AccountFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

20107a20d0ae9634f0299ffa6afbd8c4db5f386f
Titas212 <45366405+Titas212@users.noreply.github.com>
2022-05-26 17:21:30 +0200

    Final comit of the day fixed only from main removing sendig fake data to downlink

--- Embedded Tier/iotproj/src/DownLinkMessage.java
+++ Embedded Tier/iotproj/src/DownLinkMessage.java
@@ -1,16 +1,16 @@
 public class DownLinkMessage {
-    private String data;
     private String cmd;
     private String EUI;
     private int port;
     private boolean confirmed;
+    private String data;
 
-    public DownLinkMessage(String data, String cmd, String EUI, int port, boolean confirmed) {
-        this.data = data;
+    public DownLinkMessage(String cmd, String EUI, int port, boolean confirmed, String data) {
         this.cmd = cmd;
         this.EUI = EUI;
         this.port = port;
         this.confirmed = confirmed;
+        this.data = data;
     }
 
     public String getData() {

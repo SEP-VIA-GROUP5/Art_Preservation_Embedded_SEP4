a31ce95828bf54b8aeba5daa1cf1532270f27fda
felixioan <74063400+felixioan@users.noreply.github.com>
2022-05-30 10:26:57 +0200

    updateing the home fragment

--- Data Tier/sep4/src/main/java/dk/via/sep4/Sep4Application.java
+++ Data Tier/sep4/src/main/java/dk/via/sep4/Sep4Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sep4Application {
+public class
+Sep4Application {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Sep4Application.class, args);
